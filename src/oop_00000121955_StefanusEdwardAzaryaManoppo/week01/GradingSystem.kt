12306f4e16dfe07ff59eed10c50535b94fa219c7
StefanusManoppo <manoppostefanus@gmail.com>
2026-02-09 20:31:42 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
