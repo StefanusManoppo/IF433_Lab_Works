298464539815b2768887cce7e4ac21dc7bd6eb5b
StefanusManoppo <manoppostefanus@gmail.com>
2026-02-17 20:58:28 +0700

    week02: create basic Student class structure

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week02/Student.kt
@@ -0,0 +1,9 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week02
+
+class Student (
+    val name: String,
+    val nim: String,
+    var major: String
+){
+    // Body class kosong dulu
+}
