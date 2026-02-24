3c07fb4bc00674dd0836be9e7c1c08fb2c088400
StefanusManoppo <manoppostefanus@gmail.com>
2026-02-24 21:24:26 +0700

    week03: (task 2) implement Player testing in main

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week03/tugasmandiri2/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week03.tugasmandiri2
+
+fun main() {
+    val player = Player("Stefanus")
+
+    // println(player.xp)
+
+    player.addXp(50)
+    player.addXp(60)
+}
