40e39faadcf40e4d78c19373cd5dbda4cef02101
StefanusManoppo <manoppostefanus@gmail.com>
2026-02-24 21:07:57 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week03/tugasmandiri1/Weapon.kt
@@ -0,0 +1,24 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week03.tugasmandiri1
+
+class Weapon(val name: String) {
+
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Peringatan: Damage tidak boleh negatif! Nilai asli tidak diubah.")
+            } else if (value > 1000) {
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() {
+            return when {
+                damage > 800 -> "Legendary"
+                damage > 500 -> "Epic"
+                else -> "Common"
+            }
+        }
+}
