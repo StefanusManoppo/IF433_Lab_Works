6a49648778f46f931e563f247cd627ae4c16c675
StefanusManoppo <manoppostefanus@gmail.com>
2026-03-04 23:46:31 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week05
+
+abstract class Pegawai(val nama: String) {
+    // Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
