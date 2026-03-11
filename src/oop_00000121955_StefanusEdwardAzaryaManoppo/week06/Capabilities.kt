6e834d775977e1f1ed0707cb069fa784faed205e
StefanusManoppo <manoppostefanus@gmail.com>
2026-03-11 21:44:26 +0700

    week06: create compound interfaces

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week06
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
