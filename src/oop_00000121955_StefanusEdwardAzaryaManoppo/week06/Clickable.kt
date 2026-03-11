6ae2af8f71b41185bc8bc467df3e863933a2302e
StefanusManoppo <manoppostefanus@gmail.com>
2026-03-11 15:42:52 +0700

    week06: create Button class implementing Clickable

--- src/oop_00000121955_StefanusEdwardAzaryaManoppo/week06/Clickable.kt
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week06/Clickable.kt
@@ -3,4 +3,9 @@ package oop_00000121955_StefanusEdwardAzaryaManoppo.week06
 interface Clickable {
     val name: String // Abstract property
     fun click()
+    class Button(override val name: String) : Clickable {
+        override fun click() {
+            println("Tombol '$name' berhasil diklik!")
+        }
+    }
 }
