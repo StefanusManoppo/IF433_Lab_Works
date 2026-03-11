ecade72bf435350c0580808180c724feea731a50
StefanusManoppo <manoppostefanus@gmail.com>
2026-03-11 22:54:39 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week06/tugasmandiri/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week06.tugasmandiri
+
+fun main() {
+    val lampuTamu = SmartLamp("L01", "Ruang Tamu")
+    val speakerDapur = SmartSpeaker("S01", "Google Nest Dapur")
+    val cctvGarasi = SmartCCTV("C01", "Ezviz Garasi")
+}
