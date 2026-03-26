0d78b45dc8cf1f3d850826fc69e29edee3db1b64
StefanusManoppo <manoppostefanus@gmail.com>
2026-03-27 00:31:31 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_00000121955_StefanusEdwardAzaryaManoppo/week07/tugasmandiri/Main.kt
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week07/tugasmandiri/Main.kt
@@ -17,4 +17,15 @@ fun main() {
     println("Durability: ${starterWeapon.durability}")
 
     println()
+
+    println("Pergi ke Blacksmith untuk upgrade senjata...")
+    val upgradedItem = starterWeapon.item.copy(damage = 25)
+    println("Senjata di-upgrade! Damage baru: ${upgradedItem.damage}")
+    println()
+
+    println("=== Memulai Simulasi Event ===")
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgradedItem))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
