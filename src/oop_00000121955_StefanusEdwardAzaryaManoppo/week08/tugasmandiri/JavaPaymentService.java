585dd5fc8aa41e8c2b5cf593daca6b6f2239e288
StefanusManoppo <manoppostefanus@gmail.com>
2026-04-18 21:23:19 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week08/tugasmandiri/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week08.tugasmandiri;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
