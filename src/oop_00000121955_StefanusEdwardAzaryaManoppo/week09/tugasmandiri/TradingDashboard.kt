c4e8ff4a629fd4260665d466e44ac96d65ef2870
StefanusManoppo <manoppostefanus@gmail.com>
2026-04-25 22:07:57 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week09/tugasmandiri/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week09.tugasmandiri
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 5, 12.0, "OPEN"),
+        TradeLog("BTCUSDT", "SHORT", 50, -18.5, "CLOSED"),
+        TradeLog("DOGEUSDT", "LONG", 10, 45.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 20, -3.0, "OPEN")
+    )
+}
