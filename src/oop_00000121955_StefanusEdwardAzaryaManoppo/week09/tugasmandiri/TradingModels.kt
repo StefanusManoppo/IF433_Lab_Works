0a13811bb5c5784da4d870f5e0df796896d2fdf0
StefanusManoppo <manoppostefanus@gmail.com>
2026-04-25 22:05:18 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week09/tugasmandiri/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week09.tugasmandiri
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
