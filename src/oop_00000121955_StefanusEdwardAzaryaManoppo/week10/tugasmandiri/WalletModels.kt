ab92e9d96ac9ad4f7390b77904c44950448d8411
StefanusManoppo <manoppostefanus@gmail.com>
2026-04-29 22:20:54 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week10/tugasmandiri/WalletModels.kt
@@ -0,0 +1,5 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week10.tugasmandiri
+
+data class Coin(val name: String, val balance: Double)
+
+data class Transaction(val id: String, val amount: Double)
