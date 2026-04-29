070ffc24899c4a8a177fcbde30921dcd5613df9c
StefanusManoppo <manoppostefanus@gmail.com>
2026-04-29 22:43:30 +0700

    week10: (task) populate coin repository

--- src/oop_00000121955_StefanusEdwardAzaryaManoppo/week10/tugasmandiri/CryptoDashboard.kt
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week10/tugasmandiri/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000121955_StefanusEdwardAzaryaManoppo.week10.tugasmandiri
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 2000.0))
 }
