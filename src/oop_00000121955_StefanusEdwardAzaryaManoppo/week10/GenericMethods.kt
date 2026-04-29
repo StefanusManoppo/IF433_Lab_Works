6d2e1fc698a89e5e14006614d071c1ef596deb20
StefanusManoppo <manoppostefanus@gmail.com>
2026-04-29 22:02:21 +0700

    week10: implement generic function with return type

--- src/oop_00000121955_StefanusEdwardAzaryaManoppo/week10/GenericMethods.kt
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week10/GenericMethods.kt
@@ -3,3 +3,7 @@ package oop_00000121955_StefanusEdwardAzaryaManoppo.week10
 fun <T> printData(data: T) {
     println("Data yang diterima: $data")
 }
+
+fun <T> processData(input: T): T {
+    return input
+}
