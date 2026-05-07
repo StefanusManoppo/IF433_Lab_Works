3626c396984338ef054ea1ce3f72cf06a3a2bc9f
StefanusManoppo <manoppostefanus@gmail.com>
2026-05-07 22:06:02 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000121955_StefanusEdwardAzaryaManoppo/week11/tugasmandiri/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000121955_StefanusEdwardAzaryaManoppo.week11.tugasmandiri
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
